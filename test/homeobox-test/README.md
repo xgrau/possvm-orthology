768e91db49776baf618628478c23fb03584430e6
xgrau <xavier.graubove@gmail.com>
2021-02-06 22:23:46 +0100

    clean tests

---
+++ test/homeobox-test/README.md
@@ -0,0 +1,13 @@
+# TALE homeobox classification
+
+1. Same species as Orthobench
+
+2. Get TALE (ANTP) sequences from HomeoDB.
+
+3. Blast (diamond).
+
+4. Tree.
+
+5. POSSVM.
+
+6. Evaluate using classification from blast to HomeoDB.
---
+++ test/homeobox-test/s01_get_trees-diamond.sh
@@ -0,0 +1,39 @@
+# input variables
+n_cpu="8"
+input_fastas="../orthobench-data/proteomes/"
+alignments="results_trees"
+searches="results_searches"
+
+seed_fasta=$1
+ref=$2
+
+mkdir -p ${alignments}
+mkdir -p ${searches}
+
+# concatenate all proteomes
+echo "# Prepare input..."
+zcat ${input_fastas}/*.fasta.gz > ${input_fastas}/all_proteomes.fa
+esl-sfetch --index ${input_fastas}/all_proteomes.fa
+diamond makedb --in ${input_fastas}/all_proteomes.fa -d ${input_fastas}/all_proteomes.fa --quiet
+
+# DIAMOND search
+# alignments
+echo "# ${ref} | diamond search"
+diamond blastp \
+        --more-sensitive \
+	--max-target-seqs 100 \
+        -d ${input_fastas}/all_proteomes.fa \
+        -q ${seed_fasta} \
+        -o ${searches}/${ref}.seed.diamond.csv \
+        --quiet \
+        --threads ${n_cpu}
+
+# extract complete sequences
+awk '$11 < 1e-6 { print $2 }' ${searches}/${ref}.seed.diamond.csv | sort -u > ${searches}/${ref}.genes.txt
+esl-sfetch -f ${input_fastas}/all_proteomes.fa ${searches}/${ref}.genes.txt > ${alignments}/${ref}.genes.fasta
+echo "# ${ref} | found: $(wc -l ${searches}/${ref}.genes.txt)"
+
+# MAFFT, trimming & IQTREE
+echo "# ${ref} | Get tree..."
+qsub -N tree-${ref} -pe smp ${n_cpu} qsub_alignment.sh ${alignments}/${ref}.genes.fasta ${n_cpu}
+
