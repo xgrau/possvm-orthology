98e16ac87e15d6f7660c70c01192c142ad69ae1d
xgrau <xavier.graubove@gmail.com>
2021-02-06 21:12:40 +0100

    summ

--- test/orthobench-data/s02_evaluate_orthobench.R
+++ test/orthobench-data/s02_evaluate_orthobench.R
@@ -20,7 +20,6 @@ fam_list = unique(ref$refOG)
 sps_list = unique(ref$species)
 
 # store diagnostics
-dia = data.frame()
 
 #### per-family evaluation ####
 
@@ -30,6 +29,7 @@ for (ort_set in ort_sets) {
   
   ort_fo = ort_set$ort_fo
   id=ort_set$id
+  dia = data.frame()
   
   pdf(sprintf("eval_%s_classification_alluvial.pdf",id),height = 8, width = 7)
   for (fam in fam_list) {
@@ -57,14 +57,19 @@ for (ort_set in ort_sets) {
       colnames(ort_xtab) = c("refOG","Possvm","Freq")
       ort_xtab_pos = ort_xtab[ort_xtab$refOG == fam,]
       # this line will retrieve all OGs that have a shared gene with refOG (more inclusive, implies better recall)
-      ort_xtab_max = which(ort_xtab_pos$Freq>1)
-      # this line will the BEST OGs with shared genes with refOG (less inclusive, aims to identify a single hit, therefore lower recall)
-      # ort_xtab_max = which.max(ort_xtab_pos$Freq)
-      ort_equi = ort_xtab_pos[ort_xtab_max,"Possvm"]
-      ort_equi_string = paste(ort_equi,collapse = ",")
+      ort_xtab_hits = which(ort_xtab_pos$Freq>0)
+      ort_hits = ort_xtab_pos[ort_xtab_hits,"Possvm"]
+      ort_hits_string = paste(ort_hits,collapse = ",")
+      ort_hits_count = sum(ort_xtab_pos$Freq[ort_xtab_hits])
       
-      # assign bool to most likely Possvm OG
-      ort$orthogroup_bool = (ort$orthogroup %in% ort_equi)
+      # this line will the BEST OGs with shared genes with refOG (less inclusive, aims to identify a single hit, therefore lower recall)
+      ort_xtab_max = which.max(ort_xtab_pos$Freq)
+      ort_hits_best = ort_xtab_pos[ort_xtab_max,"Possvm"]
+      ort_hits_best_count = sum(ort_xtab_pos$Freq[ort_xtab_max])
+      
+      
+      # assign bool Possvm OGs
+      ort$orthogroup_bool = (ort$orthogroup %in% ort_hits)
       ort[is.na(ort$orthogroup_bool),"orthogroup_bool"] = F
       
       # evaluate
@@ -91,7 +96,7 @@ for (ort_set in ort_sets) {
       }
       title(
         main=sprintf("%s\nn=%i",fam, length(rei$gene)),
-        sub=sprintf("%s is %s\nPrecision = %.2f | Recall = %.2f | F-score = %.2f", fam, ort_equi_string,ev_precision, ev_recall, ev_Fscore),
+        sub=sprintf("%s is %s\nPrecision = %.2f | Recall = %.2f | F-score = %.2f", fam, ort_hits_string,ev_precision, ev_recall, ev_Fscore),
         cex.main=0.8,
         cex.sub=0.7)
       
@@ -100,7 +105,10 @@ for (ort_set in ort_sets) {
         dia, 
         data.frame(
           refOG = fam, 
-          best_hit=ort_equi_string, 
+          hits=ort_hits_string, 
+          num_hits=ort_hits_count,
+          best_hit=ort_hits_best,
+          num_best_hits=ort_hits_best_count,
           precision=ev_precision, 
           recall=ev_recall, 
           Fscore=ev_Fscore,
@@ -118,18 +126,49 @@ for (ort_set in ort_sets) {
   
   # compare precsion and recall
   
-  pdf(sprintf("eval_%s_summary.pdf",id),height = 6, width = 8)
+  pdf(sprintf("eval_%s_summary.pdf",id),height = 5, width = 7)
   layout(matrix(1:6, nrow = 2))
   plot(dia$precision, dia$recall, xlim = c(0,1), ylim=c(0,1), xlab = "Precision", ylab="Recall",
-       col=alpha("blue", 0.6), main="Precision & recall")
-  text(dia$precision, dia$recall, labels = dia$refOG, col=alpha("lightblue",0.8))
+       col=alpha("blue", 0.6), main="Precision & recall", cex.axis=0.9, cex.lab=0.9)
+  text(dia$precision, dia$recall, labels = dia$refOG, col=alpha("lightblue",0.8),cex=0.8)
   
-  # dists
-  hist(dia$Fscore, breaks = 10, xlim = c(0,1),main="F-score", col="blue", border = "white", xlab = "F-score")
-  hist(dia$precision, breaks = 10, xlim = c(0,1),main="Precision", col="blue", border = "white", xlab = "Precision")
-  hist(dia$recall, breaks = 10, xlim = c(0,1),main="Recall", col="blue", border = "white", xlab = "Recall")
-  plot(sort(dia$precision), col="blue", ylab = "Precision", ylim = c(0,1), main="Precision")
-  plot(sort(dia$recall), col="blue", ylab = "Recall", ylim = c(0,1), main="Recall")
+  # dists of fscore, precision and recall
+  hist(dia$Fscore, breaks = 10, xlim = c(0,1),main="F-score", col="blue", ylim=c(0,50),border = "white", xlab = "F-score", cex.axis=0.9, cex.lab=0.9)
+  title(sub=sprintf("n(>=0.95) = %i | mean = %.3f", sum(dia$Fscore >= 0.95), mean(dia$Fscore)))
+  abline(v=0.95, lty=2, col="grey")
+  
+  hist(dia$precision, breaks = 10, xlim = c(0,1),main="Precision", col="blue",ylim=c(0,50), border = "white", xlab = "Precision", cex.axis=0.9, cex.lab=0.9)
+  title(sub=sprintf("n(>=0.95) = %i | mean = %.3f", sum(dia$precision >= 0.95), mean(dia$precision)))
+  abline(v=0.95, lty=2, col="grey")
+  
+  hist(dia$recall, breaks = 10, xlim = c(0,1),main="Recall", col="blue",ylim=c(0,50), border = "white", xlab = "Recall", cex.axis=0.9, cex.lab=0.9)
+  title(sub=sprintf("n(>=0.95) = %i | mean = %.3f", sum(dia$recall >= 0.95), mean(dia$recall)))
+  abline(v=0.95, lty=2, col="grey")
+  
+  plot(sort(dia$precision), col="blue", ylab = "Precision", ylim = c(0,1), main="Precision", cex.axis=0.9, cex.lab=0.9)
+  title(sub=sprintf("n(>=0.95) = %i", sum(dia$precision >= 0.95)))
+  abline(h=0.95, lty=2, col="grey")
+  
+  plot(sort(dia$recall), col="blue", ylab = "Recall", ylim = c(0,1), main="Recall", cex.axis=0.9, cex.lab=0.9)
+  title(sub=sprintf("n(>=0.95) = %i", sum(dia$recall >= 0.95)))
+  abline(h=0.95, lty=2, col="grey")
+  
+  # number of ogs per reference group
+  plot(dia$num_hits, dia$num_best_hits, col="blue", xlab = "# genes in ref OG", ylab = "# genes in best OG", cex.axis=0.9, cex.lab=0.9,
+       xlim=c(0,100), ylim = c(0,100))
+  title(sub=sprintf("n(best >=0.9 all) = %i (%.3f | r=%.3f)\nn(best = all) = %i (p=%.3f | r=%.3f)\nn(best < all) = %i (p=%.3f | r=%.3f)", 
+                    sum(dia$num_best_hits / dia$num_hits >= 0.9), 
+                    mean(dia[dia$num_best_hits / dia$num_hits >= 0.9,"precision"]), 
+                    mean(dia[dia$num_best_hits / dia$num_hits >= 0.9,"recall"]),
+                    sum(dia$num_best_hits == dia$num_hits), 
+                    mean(dia[dia$num_best_hits == dia$num_hits,"precision"]), 
+                    mean(dia[dia$num_best_hits == dia$num_hits,"recall"]),
+                    sum(dia$num_best_hits < dia$num_hits), 
+                    mean(dia[dia$num_best_hits < dia$num_hits,"precision"]), 
+                    mean(dia[dia$num_best_hits < dia$num_hits,"recall"])
+  ),
+        cex.sub=0.6)
+  abline(a=0, b=1, lty=2, col="grey")
   
   dev.off()
   
